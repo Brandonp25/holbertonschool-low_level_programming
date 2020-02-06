964ca16d0e30eb333f1bfcc48c8b88168d4de0c5
Brandonp25 <brand.stomper@gmail.com>
2020-02-06 19:05:59 +0000

    1codeC205pm

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 
-/*/
+/**
+ *  main - tells you if random number is negative or positive
+ *
+ *  Description: Uses if else to print if # if neg/pos/zero
+ *  Return: returns zero
+*/
 int main(void)
 {
 	int n;
