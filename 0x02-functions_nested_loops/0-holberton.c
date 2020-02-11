18c85993371a24be1a29b8a97c7966a781567e47
Brandonp25 <brand.stomper@gmail.com>
2020-02-11 16:35:16 +0000

    loop1 1135am

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ *main prints Holberton and then new line
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char string[9] = "Holberton";
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(string[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
